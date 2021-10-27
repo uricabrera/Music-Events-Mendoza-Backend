ce6e3a82b20ca5abb1fd4cd01aa0dd8aec4ad5db
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-27 13:19:06 -0300

    added cloudinary plugin

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4fe07febead0816b6da70b136d7012f1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '700f3bd0-2359-4a3a-b320-182da8d78d76'
+};
