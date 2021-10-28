112777ae757bd8f9f467192fc00e012f8d0b95ee
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-28 18:01:50 -0300

    fixed error with handler

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,5 +1,4 @@
 'use strict';
-const {sanitizeEntity} = require("strapi-utils")
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -7,23 +6,4 @@ const {sanitizeEntity} = require("strapi-utils")
  */
 
 module.exports = {
-  // Get logged in users
-
-  async me(ctx){
-    const user = ctx.state.user
-
-    if(!user){
-      return ctx.badRequest(null,[{messages: [{id: "No authorization header was found"}]}])
-    }
-
-    const data = await strapi.services.events.find({
-      user: user.id
-    })
-
-    if(!data){
-      return ctx.notFound()
-    }
-
-    return sanitizeEntity(data,{model: strapi.models.events})
-  }
-};
+}
